7e012a9ea5950adac257010d55b668ea9caac0d9
Enilesi <miriam.iseline@gmail.com>
2025-04-04 23:32:56 +0300

    changed font

--- itec2025-frontend/src/pages/_app.tsx
+++ itec2025-frontend/src/pages/_app.tsx
@@ -1,17 +1,36 @@
+
 import "@/styles/globals.css";
 import theme from "@/theme";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@mui/material";
+import { ThemeProvider, CssBaseline, Stack, Toolbar } from "@mui/material";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import type { AppProps } from "next/app";
+import Navbar from "@/components/navbar";
+import { Roboto } from "next/font/google";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
+});
+
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider {...pageProps}>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <AppRouterCacheProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className={roboto.variable}>
+            <Navbar />
+            <Stack sx={{ minHeight: "100vh" }}>
+              <Toolbar />
+              <Component {...pageProps} />
+            </Stack>
+          </div>
+        </ThemeProvider>
+      </AppRouterCacheProvider>
     </ClerkProvider>
   );
 }
 
-export default MyApp;
