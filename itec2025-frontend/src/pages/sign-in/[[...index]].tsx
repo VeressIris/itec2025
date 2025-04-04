23f048aa6ca33084423112923f8f224e0ddaa07b
ramiDev <147836937+mihai888nextlab@users.noreply.github.com>
2025-04-04 22:33:07 +0300

    ..

--- itec2025-frontend/src/pages/sign-in/[[...index]].tsx
+++ itec2025-frontend/src/pages/sign-in/[[...index]].tsx
@@ -1,9 +1,21 @@
+import Navbar from "@/components/navbar";
 import { SignIn } from "@clerk/nextjs";
 
 const SignInPage = () => {
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-100">
-      <SignIn path="/sign-in" routing="path" />
+    <div>
+      <Navbar />
+
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <SignIn path="/sign-in" routing="path" />
+      </div>
     </div>
   );
 };
--- itec2025-frontend/src/pages/sign-up/[[...index]].tsx
+++ itec2025-frontend/src/pages/sign-up/[[...index]].tsx
@@ -1,5 +1,21 @@
+import Navbar from "@/components/navbar";
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div>
+      <Navbar />
+
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <SignUp path="/sign-up" routing="path" />
+      </div>
+    </div>
+  );
 }
