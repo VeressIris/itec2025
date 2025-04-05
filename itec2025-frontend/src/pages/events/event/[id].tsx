9cf15301c11ccb539a6ee77f547f3da072c73330
ramiDev <147836937+mihai888nextlab@users.noreply.github.com>
2025-04-05 12:26:05 +0300

    Auto stash before merge of "main" and "origin/main"

--- itec2025-frontend/src/pages/events/event/[id].tsx
+++ itec2025-frontend/src/pages/events/event/[id].tsx
@@ -1,7 +1,46 @@
+import { backendUrl } from "@/utils";
+import { CircularProgress } from "@mui/material";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Event() {
   const router = useRouter();
+
+  const [eventDetails, setEventDetails] = useState<{
+    title: string;
+    description: string;
+    personLimit: number;
+    date: Date;
+    class: string;
+    classTags: string[];
+    grade: string;
+    _id: string;
+  } | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (!router.query.id) return;
+    const fetchEventDetails = async () => {
+      setLoading(true);
+      const id = router.query.id as string;
+      if (!id) return;
+      try {
+        const response = await fetch(`${backendUrl}/getEvent?eventId=${id}`);
+        if (!response.ok) {
+          throw new Error("Failed to fetch event details");
+        }
+        const data = await response.json();
+        setEventDetails(data);
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchEventDetails();
+  }, [router.query.id]);
 
   return (
     <div
@@ -12,7 +51,20 @@ export default function Event() {
         justifyContent: "center",
       }}
     >
-      <div></div>
+      <div>
+        {loading && <CircularProgress size={24} sx={{ mr: 2 }} />}
+        {!loading && eventDetails && (
+          <div>
+            <h1>{eventDetails.title}</h1>
+            <p>{eventDetails.description}</p>
+            <p>Date: {new Date(eventDetails.date).toLocaleDateString()}</p>
+            <p>Class: {eventDetails.class}</p>
+            <p>Grade: {eventDetails.grade}</p>
+            <p>Person Limit: {eventDetails.personLimit}</p>
+          </div>
+        )}
+        {!loading && !eventDetails && <p>No event found</p>}
+      </div>
     </div>
   );
 }
